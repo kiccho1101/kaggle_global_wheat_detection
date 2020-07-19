70a1a7dce43b2b23ed3411b59c456d973876191f
kiccho1101 <youodf11khp@gmail.com>
2020-07-19 08:13:31 +0000

    refactor: add timer

--- cv_effdet.py
+++ cv_effdet.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from src import Config
+from src.utils import timer
 from src.factories import WheatData, WheatDataset, Transforms, Fitter
 from src.factories import (
     get_data,
@@ -23,33 +24,47 @@ WORK_DIR = "."
 INPUT_DIR = f"{WORK_DIR}/input/global-wheat-detection"
 effdet_path = f"{WORK_DIR}/input/efficientdet/efficientdet_d5-ef44aea8.pth"
 
-data: WheatData = get_data(INPUT_DIR)
-train_image_ids, train_df, val_image_ids, val_df = data.get_fold(0)
 
-train_dataset: WheatDataset = get_wheat_dataset(
-    INPUT_DIR, train_image_ids, train_df, "train", transforms.get_train_transforms()
-)
-valid_dataset: WheatDataset = get_wheat_dataset(
-    INPUT_DIR, val_image_ids, val_df, "train", transforms.get_valid_transforms()
-)
+with timer("load raw data"):
+    data: WheatData = get_data(INPUT_DIR)
 
-train_loader = get_wheat_dataloader(train_dataset, config, "train")
-valid_loader = get_wheat_dataloader(valid_dataset, config, "valid")
+for cv_num in range(3):
+    with timer(f"CV No. {cv_num}"):
 
-device = torch.device("cuda")
+        with timer("prepare dataloader and fitter"):
+            train_image_ids, train_df, val_image_ids, val_df = data.get_fold(cv_num)
 
-model = get_effdet(effdet_path)
-model.to(device)
+            train_dataset: WheatDataset = get_wheat_dataset(
+                INPUT_DIR,
+                train_image_ids,
+                train_df,
+                "train",
+                transforms.get_train_transforms(),
+            )
+            valid_dataset: WheatDataset = get_wheat_dataset(
+                INPUT_DIR,
+                val_image_ids,
+                val_df,
+                "train",
+                transforms.get_valid_transforms(),
+            )
 
-fitter: Fitter = get_fitter(
-    WORK_DIR=WORK_DIR,
-    INPUT_DIR=INPUT_DIR,
-    model=model,
-    device=device,
-    loss_fn=get_average_meter(),
-    config=config,
-)
+            train_loader = get_wheat_dataloader(train_dataset, config, "train")
+            valid_loader = get_wheat_dataloader(valid_dataset, config, "valid")
 
-fitter.fit(train_loader, valid_loader)
+            device = torch.device("cuda")
 
-# %%
+            model = get_effdet(effdet_path)
+            model.to(device)
+
+            fitter: Fitter = get_fitter(
+                WORK_DIR=WORK_DIR,
+                INPUT_DIR=INPUT_DIR,
+                model=model,
+                device=device,
+                loss_fn=get_average_meter(),
+                config=config,
+            )
+
+        with timer("fit"):
+            fitter.fit(train_loader, valid_loader)
