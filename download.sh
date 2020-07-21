b082e5f3bb2e659be41410c60b32c3c02cde13a3
kiccho1101 <youodf11khp@gmail.com>
2020-07-21 13:09:02 +0000

    add: download script

---
+++ download.sh
@@ -0,0 +1,13 @@
+cd input
+
+kaggle competitions download -c global-wheat-detection
+unzip global-wheat-detection.zip -d global-wheat-detection.zip
+rm global-wheat-detection.zip
+
+kaggle datasets download -d mathurinache/efficientdet
+unzip efficientdet.zip -d efficientdet.zip
+rm efficientdet.zip
+
+kaggle datasets download -d shonenkov/timm-efficientdet-pytorch
+unzip timm-efficientdet-pytorch.zip -d timm-efficientdet-pytorch.zip
+rm timm-efficientdet-pytorch.zip
