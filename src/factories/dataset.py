0ebd2b900d8463db06e3dbf6a191a8b470fa48f6
kiccho1101 <youodf11khp@gmail.com>
2020-07-30 23:52:32 +0000

    add: mixup

--- src/factories/dataset.py
+++ src/factories/dataset.py
@@ -1,10 +1,8 @@
-import re
 import pandas as pd
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
-from sklearn.model_selection import StratifiedKFold
+import random
 
 import torch
 from torch.utils.data import Dataset
@@ -38,21 +36,22 @@ class WheatDataset(Dataset):
         self, index: int
     ) -> Union[torch.Tensor, NDArray[(1024, 1024, 3), np.int], Dict[str, Any], str]:
         image_id: str = self.image_ids[index]
-        records: pd.DataFrame = self.df[self.df["image_id"] == image_id]
 
-        image: NDArray[(1024, 1024, 3), np.int] = self._read_image(image_id)
-        bboxes: Boxes = records[["x_min", "y_min", "x_max", "y_max"]].values
+        image: NDArray[(1024, 1024, 3), np.int] = np.array([])
+        bboxes: Boxes = np.array([])
+        if self.mode == "train" and random.random() > 0.5:
+            image, bboxes = self._load_cutmix_image_and_boxes(index)
+        else:
+            image = self._read_image(image_id)
+            bboxes = self.df[self.df["image_id"] == image_id][
+                ["x_min", "y_min", "x_max", "y_max"]
+            ].values
 
-        area: torch.Tensor = self._get_area(bboxes)
-        labels: torch.Tensor = torch.ones((records.shape[0],), dtype=torch.int64)
-        iscrowd: torch.Tensor = torch.zeros((records.shape[0],), dtype=torch.int64)
+        labels: torch.Tensor = torch.ones((bboxes.shape[0],), dtype=torch.int64)
 
         target: Dict[str, Any] = {
             "bboxes": bboxes,
-            "area": area,
             "labels": labels,
-            "iscrowd": iscrowd,
-            "mask": None,
         }
 
         if self.transforms:
@@ -80,13 +79,86 @@ class WheatDataset(Dataset):
         image /= 255.0
         return image
 
-    @staticmethod
-    def _get_area(bboxes: NDArray[(Any, 4), np.float]) -> torch.Tensor:
-        area: torch.Tensor = (bboxes[:, 3] - bboxes[:, 1]) * (
-            bboxes[:, 2] - bboxes[:, 0]
+    def _load_cutmix_image_and_boxes(
+        self, index: int, imsize: int = 1024,
+    ):
+        w, h = imsize, imsize
+        s: int = imsize // 2
+
+        xc: int = int(random.uniform(imsize * 0.25, imsize * 0.75))
+        yc: int = int(random.uniform(imsize * 0.25, imsize * 0.75))
+
+        indexes: List[int] = [index] + [
+            random.randint(0, self.image_ids.shape[0] - 1) for _ in range(3)
+        ]
+
+        result_image: NDArray[(imsize, imsize, 3), np.float32] = np.full(
+            (imsize, imsize, 3), 1, dtype=np.float32
         )
-        area = torch.as_tensor(area, dtype=torch.float32)
-        return area
+
+        result_bboxes = []
+        for i, idx in enumerate(indexes):
+            image_id: str = self.image_ids[idx]
+            image: NDArray[(1024, 1024, 3), np.int] = self._read_image(image_id)
+            bboxes: Boxes = self.df[self.df["image_id"] == image_id][
+                ["x_min", "y_min", "x_max", "y_max"]
+            ].values
+
+            x1a: int = 0
+            y1a: int = 0
+            x2a: int = 0
+            y2a: int = 0
+            x1b: int = 0
+            y1b: int = 0
+            x2b: int = 0
+            y2b: int = 0
+            if i == 0:
+                x1a, y1a, x2a, y2a = (
+                    max(xc - w, 0),
+                    max(yc - h, 0),
+                    xc,
+                    yc,
+                )  # xmin, ymin, xmax, ymax (large image)
+
+                x1b, y1b, x2b, y2b = (
+                    w - (x2a - x1a),
+                    h - (y2a - y1a),
+                    w,
+                    h,
+                )  # xmin, ymin, xmax, ymax (large image)
+
+            elif i == 1:  # top right
+                x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
+                x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
+            elif i == 2:  # bottom left
+                x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
+                x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
+            elif i == 3:  # bottom right
+                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
+                x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
+
+            result_image[y1a:y2a, x1a:x2a] = image[y1b:y2b, x1b:x2b]
+            padw = x1a - x1b
+            padh = y1a - y1b
+
+            bboxes[:, 0] += padw
+            bboxes[:, 1] += padh
+            bboxes[:, 2] += padw
+            bboxes[:, 3] += padh
+
+            result_bboxes.append(bboxes)
+
+        result_bboxes = np.concatenate(result_bboxes, 0)
+        np.clip(result_bboxes[:, 0:], 0, 2 * s, out=result_bboxes[:, 0:])
+        result_bboxes = result_bboxes.astype(np.int32)
+        result_bboxes = result_bboxes[
+            np.where(
+                (result_bboxes[:, 2] - result_bboxes[:, 0])
+                * (result_bboxes[:, 3] - result_bboxes[:, 1])
+                > 0
+            )
+        ]
+        return result_image, result_bboxes
 
 
 def get_wheat_dataset(
