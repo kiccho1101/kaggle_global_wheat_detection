d24f7bab4eb546b44ec541d4bb57ef03acaf4c10
kiccho1101 <youodf11khp@gmail.com>
2020-08-01 05:34:04 +0000

    add test loader

--- src/factories/dataset.py
+++ src/factories/dataset.py
@@ -10,42 +10,46 @@ from src.types import Boxes
 
 import albumentations as A
 
+from src.types import Img
 from typing import Optional, Any, Dict, Tuple, List, Union
-from nptyping import NDArray
 
 
 class WheatDataset(Dataset):
     def __init__(
         self,
         INPUT_DIR: str,
-        image_ids: NDArray[np.object],
+        image_ids: np.ndarray,
         df: pd.DataFrame,
         mode: str = "train",
         transforms: Optional[A.Compose] = None,
+        cutmix_ratio: float = 0.0,
     ):
         self.df: pd.DataFrame = df
-        self.image_ids: NDArray[np.object] = image_ids
+        self.image_ids: np.ndarray = image_ids
         self.mode: str = mode
         self.image_dir: str = f"{INPUT_DIR}/{mode}"
         self.transforms: Optional[A.Compose] = transforms
+        self.cutmix_ratio: float = cutmix_ratio
 
     def __len__(self) -> int:
         return self.image_ids.shape[0]
 
-    def __getitem__(
-        self, index: int
-    ) -> Union[torch.Tensor, NDArray[(1024, 1024, 3), np.int], Dict[str, Any], str]:
+    def __getitem__(self, index: int) -> Union[torch.Tensor, Img, Dict[str, Any], str]:
         image_id: str = self.image_ids[index]
 
-        image: NDArray[(1024, 1024, 3), np.int] = np.array([])
+        image: Img = np.array([])
         bboxes: Boxes = np.array([])
-        if self.mode == "train" and random.random() > 0.5:
-            image, bboxes = self._load_cutmix_image_and_boxes(index)
-        else:
+
+        if self.mode == "test":
             image = self._read_image(image_id)
-            bboxes = self.df[self.df["image_id"] == image_id][
-                ["x_min", "y_min", "x_max", "y_max"]
-            ].values
+        else:
+            if random.random() < self.cutmix_ratio:
+                image, bboxes = self._load_cutmix_image_and_boxes(index)
+            else:
+                image = self._read_image(image_id)
+                bboxes = self.df[self.df["image_id"] == image_id][
+                    ["x_min", "y_min", "x_max", "y_max"]
+                ].values
 
         labels: torch.Tensor = torch.ones((bboxes.shape[0],), dtype=torch.int64)
 
@@ -92,14 +96,12 @@ class WheatDataset(Dataset):
             random.randint(0, self.image_ids.shape[0] - 1) for _ in range(3)
         ]
 
-        result_image: NDArray[(imsize, imsize, 3), np.float32] = np.full(
-            (imsize, imsize, 3), 1, dtype=np.float32
-        )
+        result_image: Img = np.full((imsize, imsize, 3), 1, dtype=np.float32)
 
         result_bboxes = []
         for i, idx in enumerate(indexes):
             image_id: str = self.image_ids[idx]
-            image: NDArray[(1024, 1024, 3), np.int] = self._read_image(image_id)
+            image: Img = self._read_image(image_id)
             bboxes: Boxes = self.df[self.df["image_id"] == image_id][
                 ["x_min", "y_min", "x_max", "y_max"]
             ].values
@@ -164,7 +166,7 @@ class WheatDataset(Dataset):
 def get_wheat_dataset(
     INPUT_DIR: str,
     df: pd.DataFrame,
-    image_ids: NDArray[np.object],
+    image_ids: np.ndarray,
     mode: str = "train",
     transforms: Optional[A.Compose] = None,
 ):
